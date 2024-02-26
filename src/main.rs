dc6cbbbdc922c3feba51c1cbaefee55aaab5c135
Vera Gonzalez <me@vera.lgbt>
2024-02-26 03:36:27 -0500

    feat: added VertexBuffer creation from Polyhedron

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,77 @@ use paper_blade::prelude::*;
 
 #[cfg(not(target_arch = "wasm32"))]
 pub fn main() {
-    let poly = Polyhedron::cube();
-    println!("hey there!\n{:?}", poly,);
+    use three_d::{
+        degrees, radians, vec3, Camera, ClearState, Context, FrameOutput, Mat4, Program,
+        RenderStates, Srgba, VertexBuffer, Window, WindowSettings,
+    };
+
+    // Create a window (a canvas on web)
+    let window = Window::new(WindowSettings {
+        title: "Core Triangle!".to_string(),
+        #[cfg(not(target_arch = "wasm32"))]
+        max_size: Some((1280, 720)),
+        ..Default::default()
+    })
+    .unwrap();
+
+    // Get the graphics context from the window
+    let context: Context = window.gl();
+
+    // Define triangle vertices and colors
+    let positions = Polyhedron::cube().render(&context);
+
+    let colors = VertexBuffer::new_with_data(
+        &context,
+        &[
+            Srgba::RED.to_linear_srgb(),   // bottom right
+            Srgba::GREEN.to_linear_srgb(), // bottom left
+            Srgba::BLUE.to_linear_srgb(),  // top
+            Srgba::GREEN.to_linear_srgb(), // bottom left
+            Srgba::GREEN.to_linear_srgb(), // bottom left
+            Srgba::BLUE.to_linear_srgb(),  // top
+            Srgba::RED.to_linear_srgb(),   // bottom right
+            Srgba::BLUE.to_linear_srgb(),  // top
+        ],
+    );
+
+    let program = Program::from_source(
+        &context,
+        include_str!("triangle.vert"),
+        include_str!("triangle.frag"),
+    )
+    .unwrap();
+
+    let mut camera = Camera::new_perspective(
+        window.viewport(),
+        vec3(0.0, 0.0, 2.0),
+        vec3(0.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        degrees(45.0),
+        0.1,
+        10.0,
+    );
+
+    window.render_loop(move |frame_input| {
+        camera.set_viewport(frame_input.viewport);
+
+        frame_input
+            .screen()
+            // Clear the color and depth of the screen render target
+            .clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0))
+            .write(|| {
+                let time = frame_input.accumulated_time as f32;
+                program.use_uniform("model", Mat4::from_angle_y(radians(time * 0.005)));
+                program.use_uniform("viewProjection", camera.projection() * camera.view());
+                program.use_vertex_attribute("position", &positions);
+                program.use_vertex_attribute("color", &colors);
+                program.draw_arrays(
+                    RenderStates::default(),
+                    frame_input.viewport,
+                    positions.vertex_count(),
+                );
+            });
+
+        FrameOutput::default()
+    });
 }
--- src/polyhedra/assembler.rs
+++ src/polyhedra/assembler.rs
@@ -1,14 +1,31 @@
+use std::collections::HashSet;
+
+use super::*;
+
 // Keep track of many states while an operation is being performed
+#[derive(Default)]
 pub struct Assembler {
-    // pub flags
-    // pub vertices
-    // pub vert
-    // pub faces
+    pub flags: Vec<Vec<f64>>,
+    pub vertices: Vec<Vec<f64>>,
+    pub vertex_names: HashSet<String>,
+    pub face_names: HashSet<String>,
 }
 
 impl Assembler {
-    /*
-    pub fn commit() -> Polyhedron {
+    pub fn flag(&mut self, face: i32, v1: f64, v2: f64) {
+        let face = face as usize;
+        // Make sure there is a flag array for this face
+        while self.flags.len() - 1 < face {
+            self.flags.push(vec![]);
+        }
+        //self.flags[face][v1] = v2;
     }
-    */
+
+    pub fn commit(&self) -> Polyhedron {
+        Polyhedron {
+            name: String::new(),
+            faces: vec![],
+            vertices: vec![],
+        }
+    }
 }
