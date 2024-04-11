3da9676ac88af4db203e0af8435514950ef25750
Vera Gonzalez <me@vera.lgbt>
2024-04-11 04:10:16 -0400

    meow! a triangle has appeared

--- src/main.rs
+++ src/main.rs
@@ -8,105 +8,166 @@ use glow::HasContext;
 use std::{sync::Arc, time::Instant};
 
 use polyblade::prelude::{create_display, UserEvent};
+const shader_version: &str = "#version 410";
 
 fn main() {
-    let mut clear_color = [0.1, 0.1, 0.1];
+    unsafe {
+        let event_loop = EventLoopBuilder::<UserEvent>::with_user_event()
+            .build()
+            .unwrap();
+        let (gl_window, gl) = create_display(&event_loop);
+        let gl = Arc::new(gl);
 
-    let event_loop = EventLoopBuilder::<UserEvent>::with_user_event()
-        .build()
-        .unwrap();
-    let (gl_window, gl) = create_display(&event_loop);
-    let gl = Arc::new(gl);
+        let mut egui_glow = EguiGlow::new(&event_loop, gl.clone(), None, None);
 
-    let mut egui_glow = EguiGlow::new(&event_loop, gl.clone(), None, None);
+        let event_loop_proxy = Mutex::new(event_loop.create_proxy());
+        egui_glow
+            .egui_ctx
+            .set_request_repaint_callback(move |info| {
+                event_loop_proxy
+                    .lock()
+                    .send_event(UserEvent::Redraw(info.delay))
+                    .expect("Cannot send event");
+            });
 
-    let event_loop_proxy = Mutex::new(event_loop.create_proxy());
-    egui_glow
-        .egui_ctx
-        .set_request_repaint_callback(move |info| {
-            event_loop_proxy
-                .lock()
-                .send_event(UserEvent::Redraw(info.delay))
-                .expect("Cannot send event");
-        });
+        let mut repaint_delay = std::time::Duration::MAX;
 
-    let mut repaint_delay = std::time::Duration::MAX;
+        let vertex_array = gl
+            .create_vertex_array()
+            .expect("Cannot create vertex array");
+        gl.bind_vertex_array(Some(vertex_array));
 
-    let _ = event_loop.run(move |event, event_loop_window_target| {
-        match event {
-            Event::WindowEvent { event, .. } => {
-                match event {
-                    WindowEvent::CloseRequested | WindowEvent::Destroyed => {
-                        event_loop_window_target.exit();
-                        return;
-                    }
-                    WindowEvent::RedrawRequested => {
-                        let mut quit = false;
+        let program = gl.create_program().expect("Cannot create program");
 
-                        egui_glow.run(gl_window.window(), |egui_ctx| {
-                            egui::SidePanel::left("my_side_panel").show(egui_ctx, |ui| {
-                                ui.heading("Hello World!");
-                                if ui.button("Quit").clicked() {
-                                    quit = true;
-                                }
-                                ui.color_edit_button_rgb(&mut clear_color);
-                            });
-                        });
+        let (vertex_shader_source, fragment_shader_source) = (
+            r#"const vec2 verts[3] = vec2[3](
+                vec2(0.5f, 1.0f),
+                vec2(0.0f, 0.0f),
+                vec2(1.0f, 0.0f)
+            );
+            out vec2 vert;
+            void main() {
+                vert = verts[gl_VertexID];
+                gl_Position = vec4(vert - 0.5, 0.0, 1.0);
+            }"#,
+            r#"precision mediump float;
+            in vec2 vert;
+            out vec4 color;
+            void main() {
+                color = vec4(vert, 0.5, 1.0);
+            }"#,
+        );
 
-                        if quit {
+        let shader_sources = [
+            (glow::VERTEX_SHADER, vertex_shader_source),
+            (glow::FRAGMENT_SHADER, fragment_shader_source),
+        ];
+
+        let mut shaders = Vec::with_capacity(shader_sources.len());
+
+        for (shader_type, shader_source) in shader_sources.iter() {
+            let shader = gl
+                .create_shader(*shader_type)
+                .expect("Cannot create shader");
+            gl.shader_source(shader, &format!("{}\n{}", shader_version, shader_source));
+            gl.compile_shader(shader);
+            if !gl.get_shader_compile_status(shader) {
+                panic!("{}", gl.get_shader_info_log(shader));
+            }
+            gl.attach_shader(program, shader);
+            shaders.push(shader);
+        }
+
+        gl.link_program(program);
+        if !gl.get_program_link_status(program) {
+            panic!("{}", gl.get_program_info_log(program));
+        }
+
+        for shader in shaders {
+            gl.detach_shader(program, shader);
+            gl.delete_shader(shader);
+        }
+
+        gl.use_program(Some(program));
+        gl.clear_color(0.1, 0.2, 0.3, 1.0);
+
+        let _ = event_loop.run(move |event, event_loop_window_target| {
+            match event {
+                Event::WindowEvent { event, .. } => {
+                    match event {
+                        WindowEvent::CloseRequested | WindowEvent::Destroyed => {
                             event_loop_window_target.exit();
-                        } else {
-                            event_loop_window_target.set_control_flow(if repaint_delay.is_zero() {
-                                gl_window.window().request_redraw();
-                                ControlFlow::Poll
-                            } else if let Some(repaint_after_instant) =
-                                Instant::now().checked_add(repaint_delay)
-                            {
-                                ControlFlow::WaitUntil(repaint_after_instant)
-                            } else {
-                                ControlFlow::Wait
+                            return;
+                        }
+                        WindowEvent::RedrawRequested => {
+                            let mut quit = false;
+
+                            /*
+                            egui_glow.run(gl_window.window(), |egui_ctx| {
+                                egui::TopBottomPanel::bottom("my_side_panel").show(
+                                    egui_ctx,
+                                    |ui| {
+                                        ui.heading("Hello World!");
+                                        if ui.button("big chil").clicked() {
+                                            quit = true;
+                                        }
+                                    },
+                                );
                             });
-                        }
+                            */
 
-                        unsafe {
-                            use glow::HasContext as _;
-                            gl.clear_color(clear_color[0], clear_color[1], clear_color[2], 1.0);
+                            if quit {
+                                event_loop_window_target.exit();
+                            } else {
+                                event_loop_window_target.set_control_flow(
+                                    if repaint_delay.is_zero() {
+                                        gl_window.window().request_redraw();
+                                        ControlFlow::Poll
+                                    } else if let Some(repaint_after_instant) =
+                                        Instant::now().checked_add(repaint_delay)
+                                    {
+                                        ControlFlow::WaitUntil(repaint_after_instant)
+                                    } else {
+                                        ControlFlow::Wait
+                                    },
+                                );
+                            }
+
+                            gl.clear_color(0.4, 0.4, 0.4, 1.0);
                             gl.clear(glow::COLOR_BUFFER_BIT);
+
+                            // draw things behind egui here
+
+                            //egui_glow.paint(gl_window.window());
+                            gl.draw_arrays(glow::TRIANGLES, 0, 3);
+
+                            // draw things on top of egui here
+
+                            gl_window.swap_buffers().unwrap();
+                            gl_window.window().set_visible(true);
                         }
-
-                        // draw things behind egui here
-                        unsafe {
-                            //    gl.draw_arrays(glow::TRIANGLES, 0, 3);
+                        WindowEvent::Resized(physical_size) => {
+                            gl_window.resize(physical_size);
                         }
-
-                        egui_glow.paint(gl_window.window());
-
-                        // draw things on top of egui here
-
-                        gl_window.swap_buffers().unwrap();
-                        gl_window.window().set_visible(true);
+                        _ => {}
                     }
-                    WindowEvent::Resized(physical_size) => {
-                        gl_window.resize(physical_size);
+                    let event_response = egui_glow.on_window_event(gl_window.window(), &event);
+                    if event_response.repaint {
+                        gl_window.window().request_redraw();
                     }
-                    _ => {}
                 }
-                let event_response = egui_glow.on_window_event(gl_window.window(), &event);
-                if event_response.repaint {
+                Event::UserEvent(UserEvent::Redraw(delay)) => {
+                    repaint_delay = delay;
+                }
+                Event::LoopExiting => {
+                    egui_glow.destroy();
+                }
+                Event::NewEvents(StartCause::ResumeTimeReached { .. }) => {
                     gl_window.window().request_redraw();
                 }
-            }
-            Event::UserEvent(UserEvent::Redraw(delay)) => {
-                repaint_delay = delay;
-            }
-            Event::LoopExiting => {
-                egui_glow.destroy();
-            }
-            Event::NewEvents(StartCause::ResumeTimeReached { .. }) => {
-                gl_window.window().request_redraw();
-            }
 
-            _ => (),
-        }
-    });
+                _ => (),
+            }
+        });
+    }
 }
--- src/window.rs
+++ src/window.rs
@@ -2,9 +2,19 @@
 #![allow(unsafe_code)]
 #![allow(clippy::arc_with_non_send_sync)] // glow::Context was accidentally non-Sync in glow 0.13, but that will be fixed in future releases of glow: https://github.com/grovesNL/glow/commit/c4a5f7151b9b4bbb380faa06ec27415235d1bf7e
 
-use std::num::NonZeroU32;
+use std::{num::NonZeroU32, time::Duration};
 
 use egui_winit::winit;
+use glutin::{
+    config::{ConfigTemplateBuilder, GlConfig},
+    context::{ContextApi, ContextAttributesBuilder, NotCurrentGlContext, Version},
+    display::{GetGlDisplay, GlDisplay},
+    prelude::GlSurface,
+    surface::{SurfaceAttributesBuilder, WindowSurface},
+};
+use glutin_winit::DisplayBuilder;
+use rwh_05::HasRawWindowHandle;
+use winit::{dpi::LogicalSize, event_loop::EventLoopWindowTarget, window::WindowBuilder};
 
 /// The majority of `GlutinWindowContext` is taken from `eframe`
 pub struct GlutinWindowContext {
@@ -18,66 +28,48 @@ impl GlutinWindowContext {
     // refactor this function to use `glutin-winit` crate eventually.
     // preferably add android support at the same time.
     #[allow(unsafe_code)]
-    unsafe fn new(event_loop: &winit::event_loop::EventLoopWindowTarget<UserEvent>) -> Self {
-        use glutin::context::NotCurrentGlContext;
-        use glutin::display::GetGlDisplay;
-        use glutin::display::GlDisplay;
-        use glutin::prelude::GlSurface;
-        use rwh_05::HasRawWindowHandle;
-        let winit_window_builder = winit::window::WindowBuilder::new()
+    unsafe fn new(event_loop: &EventLoopWindowTarget<UserEvent>) -> Self {
+        let winit_window_builder = WindowBuilder::new()
             .with_resizable(true)
-            .with_inner_size(winit::dpi::LogicalSize {
+            .with_inner_size(LogicalSize {
                 width: 800.0,
                 height: 600.0,
             })
-            .with_title("egui_glow example") // Keep hidden until we've painted something. See https://github.com/emilk/egui/pull/2279
-            .with_visible(false);
+            .with_title("egui_glow example"); // Keep hidden until we've painted something. See https://github.com/emilk/egui/pull/2279
 
-        let config_template_builder = glutin::config::ConfigTemplateBuilder::new()
-            .prefer_hardware_accelerated(None)
-            .with_depth_size(0)
-            .with_stencil_size(0)
-            .with_transparency(false);
+        let template = ConfigTemplateBuilder::new().with_transparency(false);
+        //.prefer_hardware_accelerated(None)
+        //.with_depth_size(0)
+        //.with_stencil_size(0)
 
-        log::debug!("trying to get gl_config");
+        println!("trying to get gl_config");
         let (mut window, gl_config) =
-            glutin_winit::DisplayBuilder::new() // let glutin-winit helper crate handle the complex parts of opengl context creation
-                .with_preference(glutin_winit::ApiPreference::FallbackEgl) // https://github.com/emilk/egui/issues/2520#issuecomment-1367841150
+            DisplayBuilder::new() // let glutin-winit helper crate handle the complex parts of opengl context creation
                 .with_window_builder(Some(winit_window_builder.clone()))
-                .build(
-                    event_loop,
-                    config_template_builder,
-                    |mut config_iterator| {
-                        config_iterator.next().expect(
-                            "failed to find a matching configuration for creating glutin config",
-                        )
-                    },
-                )
+                .build(event_loop, template, |configs| {
+                    configs
+                        .reduce(|accum, config| {
+                            if config.num_samples() > accum.num_samples() {
+                                config
+                            } else {
+                                accum
+                            }
+                        })
+                        .unwrap()
+                })
                 .expect("failed to create gl_config");
         let gl_display = gl_config.display();
-        log::debug!("found gl_config: {:?}", &gl_config);
+        println!("found gl_config: {:?}", &gl_config);
 
         let raw_window_handle = window.as_ref().map(|w| w.raw_window_handle());
-        log::debug!("raw window handle: {:?}", raw_window_handle);
-        let context_attributes =
-            glutin::context::ContextAttributesBuilder::new().build(raw_window_handle);
-        // by default, glutin will try to create a core opengl context. but, if it is not available, try to create a gl-es context using this fallback attributes
-        let fallback_context_attributes = glutin::context::ContextAttributesBuilder::new()
-            .with_context_api(glutin::context::ContextApi::Gles(None))
+        println!("raw window handle: {:?}", raw_window_handle);
+        let context_attributes = ContextAttributesBuilder::new()
+            //.with_context_api(ContextApi::Gles(None))
+            .with_context_api(ContextApi::OpenGl(Some(Version { major: 3, minor: 3 })))
             .build(raw_window_handle);
-        let not_current_gl_context = unsafe {
-            gl_display
-                    .create_context(&gl_config, &context_attributes)
-                    .unwrap_or_else(|_| {
-                        log::debug!("failed to create gl_context with attributes: {:?}. retrying with fallback context attributes: {:?}",
-                            &context_attributes,
-                            &fallback_context_attributes);
-                        gl_config
-                            .display()
-                            .create_context(&gl_config, &fallback_context_attributes)
-                            .expect("failed to create context even with fallback attributes")
-                    })
-        };
+        let not_current_gl_context = gl_display
+            .create_context(&gl_config, &context_attributes)
+            .unwrap();
 
         // this is where the window is created, if it has not been created while searching for suitable gl_config
         let window = window.take().unwrap_or_else(|| {
@@ -88,18 +80,18 @@ impl GlutinWindowContext {
         let (width, height): (u32, u32) = window.inner_size().into();
         let width = NonZeroU32::new(width).unwrap_or(NonZeroU32::MIN);
         let height = NonZeroU32::new(height).unwrap_or(NonZeroU32::MIN);
-        let surface_attributes =
-            glutin::surface::SurfaceAttributesBuilder::<glutin::surface::WindowSurface>::new()
-                .build(window.raw_window_handle(), width, height);
+        let surface_attributes = SurfaceAttributesBuilder::<WindowSurface>::new().build(
+            window.raw_window_handle(),
+            width,
+            height,
+        );
         log::debug!(
             "creating surface with attributes: {:?}",
             &surface_attributes
         );
-        let gl_surface = unsafe {
-            gl_display
-                .create_window_surface(&gl_config, &surface_attributes)
-                .unwrap()
-        };
+        let gl_surface = gl_display
+            .create_window_surface(&gl_config, &surface_attributes)
+            .unwrap();
         log::debug!("surface created successfully: {gl_surface:?}.making context current");
         let gl_context = not_current_gl_context.make_current(&gl_surface).unwrap();
 
@@ -123,7 +115,6 @@ impl GlutinWindowContext {
     }
 
     pub fn resize(&self, physical_size: winit::dpi::PhysicalSize<u32>) {
-        use glutin::surface::GlSurface;
         self.gl_surface.resize(
             &self.gl_context,
             physical_size.width.try_into().unwrap(),
@@ -132,33 +123,27 @@ impl GlutinWindowContext {
     }
 
     pub fn swap_buffers(&self) -> glutin::error::Result<()> {
-        use glutin::surface::GlSurface;
         self.gl_surface.swap_buffers(&self.gl_context)
     }
 
     pub fn get_proc_address(&self, addr: &std::ffi::CStr) -> *const std::ffi::c_void {
-        use glutin::display::GlDisplay;
         self.gl_display.get_proc_address(addr)
     }
 }
 
 #[derive(Debug)]
 pub enum UserEvent {
-    Redraw(std::time::Duration),
+    Redraw(Duration),
 }
 
-pub fn create_display(
+pub unsafe fn create_display(
     event_loop: &winit::event_loop::EventLoopWindowTarget<UserEvent>,
 ) -> (GlutinWindowContext, glow::Context) {
-    let glutin_window_context = unsafe { GlutinWindowContext::new(event_loop) };
-    let gl = unsafe {
-        glow::Context::from_loader_function(|s| {
-            let s = std::ffi::CString::new(s)
-                .expect("failed to construct C string from string for gl proc address");
-
-            glutin_window_context.get_proc_address(&s)
-        })
-    };
+    let glutin_window_context = GlutinWindowContext::new(event_loop);
+    let gl = glow::Context::from_loader_function(|s| {
+        let s = std::ffi::CString::new(s).expect("C string from string for gl proc address");
+        glutin_window_context.get_proc_address(&s)
+    });
 
     (glutin_window_context, gl)
 }
