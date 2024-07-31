376c6e787d53fb047b5c3cc23beccc093bca9dc6
Vera Gonzalez <me@vera.lgbt>
2024-07-31 09:28:01 -0400

    compiling

--- src/main.rs
+++ src/main.rs
@@ -413,43 +413,21 @@ impl_scope! {
     #[widget]
     struct Polyblade {
         core: widget_core!(),
-        alpha: DVec2,
-        delta: DVec2,
-        view_delta: DVec2,
-        view_alpha: f64,
-        rel_width: f32,
-        iters: i32,
+        polyhedron: PolyGraph,
+        size: f32,
     }
 
     impl Polyblade {
         fn new() -> Self {
             Polyblade {
                 core: Default::default(),
-                alpha: DVec2(1.0, 0.0),
-                delta: DVec2(-0.5, 0.0),
-                view_delta: DVec2::ZERO,
-                view_alpha: 0.0,
-                rel_width: 0.0,
-                iters: 64,
+                polyhedron: PolyGraph::icosahedron(),
+                size: 1.0,
             }
         }
 
         fn reset_view(&mut self) {
-            self.alpha = DVec2(1.0, 0.0);
-            self.delta = DVec2(-0.5, 0.0);
-        }
-
-        fn loc(&self) -> String {
-            let d0 = self.delta.0;
-            let op = if self.delta.1 < 0.0 { "−" } else { "+" };
-            let d1 = self.delta.1.abs();
-            let s = self.alpha.sum_square().sqrt().ln();
-            #[cfg(not(feature = "shader64"))] {
-                format!("Location: {:.7} {} {:.7}i; scale: {:.2}", d0, op, d1, s)
-            }
-            #[cfg(feature = "shader64")] {
-                format!("Location: {:.15} {} {:.15}i; scale: {:.2}", d0, op, d1, s)
-            }
+            self.size = 1.0;
         }
     }
 
@@ -463,18 +441,12 @@ impl_scope! {
         #[inline]
         fn set_rect(&mut self, _: &mut ConfigCx, rect: Rect) {
             self.core.rect = rect;
-            let size = DVec2::conv(rect.size);
-            let rel_width = DVec2(size.0 / size.1, 1.0);
-            self.view_alpha = 2.0 / size.1;
-            self.view_delta = -(DVec2::conv(rect.pos) * 2.0 + size) / size.1;
-            self.rel_width = rel_width.0 as f32;
         }
 
         fn draw(&mut self, mut draw: DrawCx) {
             let draw = draw.draw_device();
             let draw = DrawIface::<DrawPipe<Pipe>>::downcast_from(draw).unwrap();
-            let p = (self.alpha, self.delta, self.rel_width, self.iters);
-            draw.draw.custom(draw.get_pass(), self.core.rect, p);
+            draw.draw.custom(draw.get_pass(), self.core.rect, self.polyhedron.clone());
         }
     }
 
@@ -486,7 +458,7 @@ impl_scope! {
         }
 
         fn update(&mut self, _: &mut ConfigCx, data: &i32) {
-            self.iters = *data;
+            self.polyhedron.update();
         }
 
         fn navigable(&self) -> bool {
@@ -498,18 +470,9 @@ impl_scope! {
                 Event::Command(cmd, _) => {
                     match cmd {
                         Command::Home | Command::End => self.reset_view(),
-                        Command::PageUp => self.alpha = self.alpha / 2f64.sqrt(),
-                        Command::PageDown => self.alpha = self.alpha * 2f64.sqrt(),
+                        Command::PageUp => {},
+                        Command::PageDown => {},
                         cmd => {
-                            let d = 0.2;
-                            let delta = match cmd {
-                                Command::Up => DVec2(0.0, -d),
-                                Command::Down => DVec2(0.0, d),
-                                Command::Left => DVec2(-d, 0.0),
-                                Command::Right => DVec2(d, 0.0),
-                                _ => return Unused,
-                            };
-                            self.delta += self.alpha.complex_mul(delta);
                         }
                     }
                     cx.push(ViewUpdate);
@@ -519,24 +482,9 @@ impl_scope! {
                         event::ScrollDelta::LineDelta(_, y) => -0.5 * y as f64,
                         event::ScrollDelta::PixelDelta(coord) => -0.01 * coord.1 as f64,
                     };
-                    self.alpha = self.alpha * 2f64.powf(factor);
                     cx.push(ViewUpdate);
                 }
                 Event::Pan { alpha, delta } => {
-                    // Our full transform (from screen coordinates to world coordinates) is:
-                    // f(p) = α_w * α_v * p + α_w * δ_v + δ_w
-                    // where _w indicate world transforms (self.alpha, self.delta)
-                    // and _v indicate view transforms (see notes in PipeWindow::invoke).
-                    //
-                    // To adjust the world offset (in reverse), we use the following formulae:
-                    // α_w' = (1/α) * α_w
-                    // δ_w' = δ_w - α_w' * α_v * δ + (α_w - α_w') δ_v
-                    // where x' is the "new x".
-                    let new_alpha = self.alpha.complex_div(alpha);
-                    self.delta = self.delta - new_alpha.complex_mul(delta) * self.view_alpha
-                        + (self.alpha - new_alpha).complex_mul(self.view_delta);
-                    self.alpha = new_alpha;
-
                     cx.push(ViewUpdate);
                 }
                 Event::PressStart { press } => {
@@ -579,7 +527,7 @@ impl_scope! {
         fn new() -> PolybladeUI {
             PolybladeUI {
                 core: Default::default(),
-                label: format_data!(mbrot: &Polyblade, "{}", mbrot.loc()),
+                label: format_data!(mbrot: &Polyblade, "{}", "meow, change me"),
                 iters_label: format_value!("{}")
                     .with_min_size_em(3.0, 0.0),
                 slider: Slider::up(0..=256, |_, iters| *iters)
