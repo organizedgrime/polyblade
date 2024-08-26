2d5b9adbd4c054b2f8fe200050f6c9710cb5972a
Vera Gonzalez <me@vera.lgbt>
2024-08-26 05:58:45 -0400

    WASM (#31)
    * fixed rand dependency; added index html
    
    * fixed Instant for web compilation
    
    * fixed bin name
    
    * added first WASM build
    
    * updated README

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 mod bones;
 mod render;
-use iced::Application as _;
+use iced::{Application as _, Settings};
 use render::Polyblade;
 
+#[cfg(target_arch = "wasm32")]
+pub use iced::time::Instant;
+#[cfg(not(target_arch = "wasm32"))]
+pub use std::time::Instant;
+
 fn main() -> iced::Result {
-    Polyblade::run(iced::Settings::default())
+    #[cfg(target_arch = "wasm32")]
+    {
+        console_log::init().expect("Initialize logger");
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    tracing_subscriber::fmt::init();
+
+    Polyblade::run(Settings::default())
 }
